b3ca947146b2d2bb81e2dfa83ae640c54b457dbb
patulacci <thomas.patouillaud@gmail.com>
2014-09-02 11:56:47 +0200

    initialize goodbye

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+MmdBye::Application.config.secret_key_base = 'e7cc3e803e7984066ef2a550ca840a1adf93fd3e40c64439fa8cb95970b2ee45dcea8a72bd6ac8134b3eeeb948bde80fa5eddd896ed439314efa1b4b94a30141'
